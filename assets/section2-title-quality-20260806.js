ef34eca5f614d838ad5b643fb270722f52ad36d6
nachchatmahmoud-png <nachchatmahmoud@gmail.com>
2026-08-06 00:38:13 +0100

    Modifier uniquement le titre de la section 2

---
+++ assets/section2-title-quality-20260806.js
@@ -0,0 +1,39 @@
+(() => {
+  const targetTitle = "القسم الثاني: جودة المعلومات الرسمية المتعلقة بالبرنامج";
+  const previousTitles = [
+    "القسم الثاني: المعلومات حول البرنامج",
+    "القسم الثاني: شفافية المعلومات الرسمية المتعلقة بالبرنامج",
+  ];
+
+  function applySectionTwoTitle() {
+    const walker = document.createTreeWalker(
+      document.body,
+      NodeFilter.SHOW_TEXT,
+    );
+
+    let node;
+    while ((node = walker.nextNode())) {
+      if (!node.nodeValue) continue;
+
+      let value = node.nodeValue;
+      for (const previousTitle of previousTitles) {
+        if (value.includes(previousTitle)) {
+          value = value.replaceAll(previousTitle, targetTitle);
+        }
+      }
+
+      if (value !== node.nodeValue) {
+        node.nodeValue = value;
+      }
+    }
+  }
+
+  applySectionTwoTitle();
+
+  const observer = new MutationObserver(applySectionTwoTitle);
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+    characterData: true,
+  });
+})();
