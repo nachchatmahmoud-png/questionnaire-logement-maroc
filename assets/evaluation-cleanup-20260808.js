7486d453ed92e81f608f87852a22788662e97131
nachchatmahmoud-png <nachchatmahmoud@gmail.com>
2026-08-08 17:04:44 +0100

    Remove general acceptance item and simplify impact section title

--- assets/evaluation-cleanup-20260808.js
+++ assets/evaluation-cleanup-20260808.js
@@ -1,7 +1,13 @@
 (() => {
-  if (typeof G === 'undefined' || !G.accept?.[0]?.[1]) return;
+  if (typeof G === 'undefined') return;
 
-  G.accept[0][1] = G.accept[0][1].filter(([id]) => id !== 'accept_4');
+  if (G.accept?.[0]?.[1]) {
+    G.accept[0][1] = G.accept[0][1].filter(([id]) => id !== 'accept_4');
+  }
+
+  if (G.generalImpact?.[0]) {
+    G.generalImpact[0][0] = 'رابعًا: الأثر العام للبرنامج';
+  }
 
   if (typeof state !== 'undefined' && state.a) {
     delete state.a.accept_4;
