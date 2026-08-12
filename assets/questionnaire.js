b506aa6ae3adba6748f9b162d9a135791fc254ae
nachchatmahmoud-png <nachchatmahmoud@gmail.com>
2026-08-12 15:11:41 +0100

    fix: allow Apps Script cold-start latency

--- assets/questionnaire.js
+++ assets/questionnaire.js
@@ -269,7 +269,7 @@ function callAuthBridge(action,idToken,extra={}){
   add('requestId',requestId);add('action',action);add('idToken',idToken);
   Object.entries(extra).forEach(([name,value])=>add(name,String(value??'')));
   const cleanup=()=>{form.remove();frame.remove();};
-  const timer=setTimeout(()=>{authRequests.delete(requestId);cleanup();reject(new Error('request_timeout'));},25000);
+  const timer=setTimeout(()=>{authRequests.delete(requestId);cleanup();reject(new Error('request_timeout'));},45000);
   authRequests.set(requestId,{resolve:result=>{clearTimeout(timer);cleanup();resolve(result);}});
   document.body.append(frame,form);
   form.submit();
